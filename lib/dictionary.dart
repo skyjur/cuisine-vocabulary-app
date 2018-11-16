2aadc98e6e4ae5efefb8d7bc237a8a166fa37cee
Ski <skyjur@gmail.com>
2018-11-16 13:02:30 +0000

    Start food dictionary

---
+++ lib/dictionary.dart
@@ -0,0 +1,25 @@
+import 'package:meta/meta.dart';
+
+class Term {
+  final String term;
+  final String pinYin;
+  final String translation;
+  final String description;
+  final String imagePath;
+
+  const Term(
+      {@required this.term,
+      @required this.pinYin,
+      @required this.translation,
+      @required this.description,
+      @required this.imagePath});
+}
+
+const Dictionary = <Term>[
+  Term(
+      term: '鮮肉餛飩乾麵',
+      pinYin: 'Xiān ròu húntún gān miàn',
+      translation: 'Pork-Wonton dry noodle',
+      description: '',
+      imagePath: 'food_pictures/wonton_pork_dry_noodle.jpg')
+];
